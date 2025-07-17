3586985afd1cb611e96d7f6a944086af959c99d7
gittyhubbycoder <140948330+gittyhubbycoder@users.noreply.github.com>
2025-07-17 13:17:56 +0100

    Update update.py

--- update.py
+++ update.py
@@ -4,7 +4,7 @@ rss_url = "https://feeds.bbci.co.uk/news/rss.xml"
 rss = requests.get(rss_url).text
 
 # Extract titles using regex
-titles = re.findall(r"<title>(.*?)</title>", rss)[1:6]  # Skip the first <title> (BBC News - Home)
+titles = re.findall(r"<title>(.*?)</title>", rss)[1:8]  # Skip the first <title> (BBC News - Home)
 print("I'm running!!!")
 # Save to JSON
 with open("deploy/bbc.json", "w", encoding="utf-8") as f:
