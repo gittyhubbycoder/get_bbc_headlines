399682da013724d257fc019e521a3ac22b34ee97
gittyhubbycoder <140948330+gittyhubbycoder@users.noreply.github.com>
2025-07-17 11:50:43 +0100

    Update update.py

--- update.py
+++ update.py
@@ -5,7 +5,7 @@ rss = requests.get(rss_url).text
 
 # Extract titles using regex
 titles = re.findall(r"<title>(.*?)</title>", rss)[1:6]  # Skip the first <title> (BBC News - Home)
-
+print("I'm running!!!")
 # Save to JSON
 with open("bbc.json", "w", encoding="utf-8") as f:
     json.dump({"titles": titles}, f, indent=2)
