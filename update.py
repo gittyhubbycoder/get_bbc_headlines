9d8cead8b67dacb58317cde8772f67ce9e615f90
gittyhubbycoder <140948330+gittyhubbycoder@users.noreply.github.com>
2025-07-17 11:52:31 +0100

    Update update.py

--- update.py
+++ update.py
@@ -7,5 +7,5 @@ rss = requests.get(rss_url).text
 titles = re.findall(r"<title>(.*?)</title>", rss)[1:6]  # Skip the first <title> (BBC News - Home)
 print("I'm running!!!")
 # Save to JSON
-with open("bbc.json", "w", encoding="utf-8") as f:
+with open("deploy/bbc.json", "w", encoding="utf-8") as f:
     json.dump({"titles": titles}, f, indent=2)
